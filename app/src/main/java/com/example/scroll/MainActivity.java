517d9ec8c6a70b1fa14107b382f16eee2bb31ef2
Vasya <br2876@bs.amalnet.k12.il>
2026-01-28 13:52:08 +0200

    setting the ui plus version

--- app/src/main/java/com/example/scroll/MainActivity.java
+++ app/src/main/java/com/example/scroll/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.scroll;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText a1;
+    EditText q;
+    Button sent;
+    Switch type;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        a1 = findViewById(R.id.a1);
+        q = findViewById(R.id.q);
+        sent = findViewById(R.id.sent);
+        type = findViewById(R.id.swich);
+
     }
+
+
+
+
+
+
 }
